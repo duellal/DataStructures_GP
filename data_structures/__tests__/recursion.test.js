ba2c33b4a7a28eb47f38f062d1bae59ce24af542
Jessica Duell <jessica.duell@pendo.io>
2023-02-01 16:07:51 -0500

    add recursion tests

--- data_structures/__tests__/recursion.test.js
+++ data_structures/__tests__/recursion.test.js
@@ -1,3 +1,14 @@
-it('',()=>{
-    expect(true).toBe(true)
+const {factorial} = require('../recursion')
+
+describe('factorial recursion', ()=>{
+    it('returns 1 for input of 0 and 1', ()=>{
+        expect(factorial(0)).toBe(1);
+        expect(factorial(1)).toBe(1);
+    })
+    
+    it('returns correct factorial for input', ()=>{
+        expect(factorial(2)).toBe(2);
+        expect(factorial(5)).toBe(120);
+        expect(factorial(10)).toBe(3628800);
+    })
 })
